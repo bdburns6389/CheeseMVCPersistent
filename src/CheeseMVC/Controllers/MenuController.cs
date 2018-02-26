da5ec06800f0b84b70c14bb8ac9155f1adea29b6
Brian <bdburns6389@gmail.com>
2018-02-26 12:21:16 -0600

    Working on Views/Menu/Index

---
+++ src/CheeseMVC/Controllers/MenuController.cs
@@ -0,0 +1,27 @@
+﻿using CheeseMVC.Data;
+using CheeseMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CheeseMVC.Controllers
+{
+    public class MenuController : Controller
+    {
+        private readonly CheeseDbContext context;
+
+        public MenuController(CheeseDbContext dbContext)
+        {
+            context = dbContext;
+        }
+
+
+        public IActionResult Index()
+        {//Might be wrong context.  Might need to be context.Menus.
+            List<CheeseMenu> categories = context.CheeseMenus.ToList();
+            return View(categories);
+        }
+    }
+}
