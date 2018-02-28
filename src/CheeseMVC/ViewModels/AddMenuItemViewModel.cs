c2ff84488dc58d40ec503662526d8125a300f873
Brian <bdburns6389@gmail.com>
2018-02-28 16:18:56 -0600

    Added Display Name for CheeseID in AddMenuItemViewModel

--- src/CheeseMVC/ViewModels/AddMenuItemViewModel.cs
+++ src/CheeseMVC/ViewModels/AddMenuItemViewModel.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -13,6 +14,7 @@ namespace CheeseMVC.ViewModels
         public List<SelectListItem> Cheeses { get; set; }
 
         public int MenuID { get; set; }
+        [Display(Name = "Cheese")]//Add to make list Not say CheeseID(Not a great look)
         public int CheeseID { get; set; }
 
 
