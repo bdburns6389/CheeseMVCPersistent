9a3ff81920f863f3128a149a09f7359cdbfe1aa8
Brian <bdburns6389@gmail.com>
2018-02-27 20:03:41 -0600

    Testing now

--- src/CheeseMVC/Controllers/MenuController.cs
+++ src/CheeseMVC/Controllers/MenuController.cs
@@ -75,6 +75,33 @@ namespace CheeseMVC.Controllers
             return View(new AddMenuItemViewModel(menu, cheeses));
         }
 
+        [HttpPost]
+        public IActionResult AddItem(AddMenuItemViewModel addMenuItemViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var cheeseID = addMenuItemViewModel.CheeseID;
+                var menuID = addMenuItemViewModel.MenuId;
 
+                IList<CheeseMenu> existingItems = context.CheeseMenus
+                    .Where(cm => cm.CheeseID == cheeseID)
+                    .Where(cm => cm.MenuID == menuID).ToList();
+
+                if (existingItems.Count == 0)
+                {
+                    CheeseMenu menuItem = new CheeseMenu
+                    {
+                        Cheese = context.Cheeses.Single(c => c.ID == cheeseID),
+                        Menu = context.Menus.Single(m => m.ID == menuID)
+                    };
+
+                    context.CheeseMenus.Add(menuItem);
+                    context.SaveChanges();
+
+                    return Redirect(string.Format("/Menu/ViewMenu/{0}", addMenuItemViewModel)); //TODO not sure where to redirect.
+                }
+            }
+            return View(addMenuItemViewModel);
+        }
     }
 }
